6b47b35a416cd2d347cc86948dd0c59152498f23
hello <hello@gmail.com>
2024-09-19 13:21:46 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    // data, functions
+    // template: '<h2>Hello world</h2>'
+    data(){
+        return {
+            title: "intro vue home",
+            author: "anonym",
+        }
+    }
+})
+
+app.mount('#app')
